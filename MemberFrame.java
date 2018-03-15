657ea211b0fcb551546426f0c8123883663cdc8f
DaHee Kim <34751044+dahkimr@users.noreply.github.com>
2018-03-15 00:29:40 -0400

    Add files via upload

--- MemberFrame.java
+++ MemberFrame.java
@@ -9,17 +9,33 @@ public class MemberFrame extends JFrame {
 	
 	public MemberFrame() {
 		
-		//get name using some method
-		JLabel name = new JLabel("Name");
+		//create the panels
+		JPanel titlePanel = createTitlePanel();
 		JPanel msgPanel = createMsgPanel();
 		JPanel rmdPanel = createRmdPanel();
-		JPanel panel = new JPanel();
 		
-		add(name, BorderLayout.NORTH);
+		add(titlePanel, BorderLayout.NORTH);
 		add(msgPanel, BorderLayout.CENTER);
 		add(rmdPanel, BorderLayout.SOUTH);
 		
 		setSize(FRAME_WIDTH, FRAME_HEIGHT);
+	}
+	
+	/**
+	   Creates panel for the title.
+	   @return the title panel.
+	 */
+	public JPanel createTitlePanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		
+		//get name using some method
+		String nameStr = " Name";
+		JLabel name = new JLabel(nameStr);
+		name.setFont(new Font("Serif", Font.BOLD, 30));
+		
+		panel.add(name);
+		return panel;
 	}
 	
 	/**
@@ -29,10 +45,12 @@ public class MemberFrame extends JFrame {
 	public JPanel createMsgPanel() {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
-		panel.setBorder(new TitledBorder(new EtchedBorder(), "Messages"));
+		panel.setBorder(new TitledBorder(new EtchedBorder(), "Messages", 
+				TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font("Serif", Font.PLAIN, 25)));
 		
 		//(to be added) Read in messages and add to textArea. 
-		JTextArea msgArea = new JTextArea(25, 5);
+		JTextArea msgArea = new JTextArea();
+		msgArea.setPreferredSize(new Dimension (400, 400));
 		msgArea.setEditable(false);
 		
 		panel.add(msgArea);
@@ -46,10 +64,12 @@ public class MemberFrame extends JFrame {
 	public JPanel createRmdPanel() {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
-		panel.setBorder(new TitledBorder(new EtchedBorder(), "Reminders"));
+		panel.setBorder(new TitledBorder(new EtchedBorder(), "Reminders", 
+				TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font("Serif", Font.PLAIN, 25)));
 		
 		//(to be added) Read in reminders and add to textArea. 
-		JTextArea rmdArea = new JTextArea(15, 20);
+		JTextArea rmdArea = new JTextArea();
+		rmdArea.setPreferredSize(new Dimension (400, 200));
 		rmdArea.setEditable(false);
 		
 		panel.add(rmdArea);
